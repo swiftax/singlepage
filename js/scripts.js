bb46ea51de3f99966f26e5df1a1ab65bd6cfa650
Anthony Tropeano <tony@swiftax.com>
2015-06-15 16:20:22 -0400

    Commit

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,40 @@ $("#submit-button").on("click" , function() {
 	$("#visible-comment").html(comment);
 	return false;
 	})
+
 	$(".message-box").css("background-color", "pink");
+
+	$(".message-box").on("keyup", function() {
+		console.log("keyup happened!");
+
+		var nameName = 3;
+		var name = "string";
+
+		var charCount = $(".message-box").val().length;
+		console.log(charCount);
+		$("#charCount").html(charCount);
+
+		if (charCount > 50) {
+			$("#charCount").css("color", "red");
+			$("#charCount").text("You exceeded the maximum character limit");
+		} else {
+			$("#charCount").css("color", "white");
+		};
+	})
+
+	$("#email").on("keyup", function() {
+		console.log("keyup with email!");
+		var charCount = $("#email").val().length
+		console.log(charCount);
+		$("#charCount").html(charCount);
+		if (charCount > 30) {
+			$("#charCount").css("color", "red");
+			$("#charCount").text("Invalid Email Address");
+		} else {
+			$("#charCount").css("color", "pink");
+		};
+	});
+
 });
 
 
